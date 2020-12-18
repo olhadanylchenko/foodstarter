792800bb2f6cdf729c458fbcea814cd9b164b132
Olha Danylchenko <danylchenkoolha@gmail.com>
2020-12-18 22:47:18 +0100

    create empty users state

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import React, { useState } from "react";
+
 function App() {
+  const [users, setUsers] = useState([]);
+
   fetch("https://jsonplaceholder.typicode.com/posts/1/comments")
-    .then((response) => response.json())
+      .then((response) => response.json())
     .then((json) => console.log(json));
 
   return <div></div>;
