11eed98676af87b9ef5e62bd7df7bd15bb355c71
Olha Danylchenko <danylchenkoolha@gmail.com>
2020-12-19 12:06:44 +0100

    render list of albums

--- src/Albums.jsx
+++ src/Albums.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 export default function Albums(props) {
   const [albums, setAlbums] = useState([]);
 
-  const id = props.match.params.id;
+  const userId = props.match.params.id;
   useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/user/${id}/albums`)
+    fetch(`https://jsonplaceholder.typicode.com/user/${userId}/albums`)
       .then((response) => response.json())
       .then((albums) => {
         const albumsList = albums.map((album) => ({
@@ -14,8 +15,21 @@ export default function Albums(props) {
         }));
         setAlbums(albumsList);
       });
-  }, [id]);
-  console.log(albums);
+  }, [userId]);
 
-  return <h1>Hi</h1>;
+  //console.log(albums);
+  return (
+    <>
+      <h1>Albums</h1>
+      <ul>
+        {albums.map((album) => (
+          <li key={album.id}>
+            <Link to={`/user/${userId}/album/${album.id}`}>
+              <h2>{album.title}</h2>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
