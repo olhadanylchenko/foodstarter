7c032c5f9c070ed1436a2119d34383ba0d42ec9f
Olha Danylchenko <danylchenkoolha@gmail.com>
2020-12-19 11:42:52 +0100

    fetch and save albums to state

--- src/Albums.jsx
+++ src/Albums.jsx
@@ -1,5 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-export default function Albums() {
+export default function Albums(props) {
+  const [albums, setAlbums] = useState([]);
+
+  const id = props.match.params.id;
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/user/${id}/albums`)
+      .then((response) => response.json())
+      .then((albums) => {
+        const albumsList = albums.map((album) => ({
+          id: album.id,
+          title: album.title,
+        }));
+        setAlbums(albumsList);
+      });
+  }, [id]);
+  console.log(albums);
+
   return <h1>Hi</h1>;
 }
