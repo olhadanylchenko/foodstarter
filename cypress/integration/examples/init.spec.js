b659d07b80d9a3442f4cd41bd806f464f76cdd15
Olha Danylchenko <danylchenkoolha@gmail.com>
2020-12-19 15:10:50 +0100

    write end-to-end cypress test

---
+++ cypress/integration/examples/init.spec.js
@@ -0,0 +1,19 @@
+describe("Cypress", () => {
+  it("is working", () => {
+    cy.visit("/");
+    cy.get("ul>li>a").first().click();
+
+    cy.location("pathname").should("include", "user");
+    cy.get("ul>li>a").first().click();
+
+    cy.location("pathname").should("include", "album");
+
+    cy.go("back");
+    cy.location("pathname").should("not.include", "album");
+
+    cy.go("forward");
+    cy.location("pathname").should("include", "album");
+
+    cy.get("li").first().get("figure").find("img").should("have.attr", "src");
+  });
+});
