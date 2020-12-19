50c04c1ef7841837c36f156019f01e2ab886e102
Olha Danylchenko <danylchenkoolha@gmail.com>
2020-12-19 10:22:53 +0100

    add react-router-dom; add BrouserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,15 @@
-import React, { useState, useEffect } from "react";
-import User from "./User";
+import React from "react";
+import Home from "./Home";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-function App() {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((users) => {
-        const usersNames = users.map((user) => ({
-          name: user.name,
-          address: user.address,
-        }));
-        setUsers(usersNames);
-      });
-  }, []);
-
+export default function App() {
   return (
-    <>
-      <h1>Users</h1>
-      <ul>
-        {users.map((user) => (
-          <User user={user} />
-        ))}
-      </ul>
-    </>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
-
-export default App;
